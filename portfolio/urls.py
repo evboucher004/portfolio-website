a0267ee9ecf835f84e9559904f8d24d4d3b3829a
Evan Boucher <evboucher004@gmail.com>
2025-12-03 02:29:00 -0500

    Added pages for each project

--- portfolio/urls.py
+++ portfolio/urls.py
@@ -2,5 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.post_list, name='post_list'),
+    path('', views.index, name='index'),
+    path('imsp/', views.imsp, name='imsp'),
+    path('mtg/', views.mtg, name='mtg'),
+    path('snhu/', views.snhu, name='snhu'),
 ]
--- portfolio/views.py
+++ portfolio/views.py
@@ -5,3 +5,16 @@ from .models import Post
 def post_list(request):
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
     return render(request, 'portfolio/post_list.html', {'posts': posts})
+
+
+def index(request):
+    return render(request, 'portfolio/index.html')
+
+def imsp(request):
+    return render(request, 'portfolio/imsp.html')
+
+def mtg(request):
+    return render(request, 'portfolio/mtg.html')
+
+def snhu(request):
+    return render(request, 'portfolio/snhu.html')
